65b97449ebaac0ac4aee895023f51c01b94d4802
Stanley Nathanael Wijaya <stanley.n.wijaya7@gmail.com>
2025-06-08 21:16:58 +0700

    Change AI API

--- src/utility/api.ts
+++ src/utility/api.ts
@@ -60,7 +60,7 @@ interface BackendClassificationItem {
 export async function classifyCommentText(
   commentText: string
 ): Promise<CommentClassificationResult> {
-    const response = await fetch("http://127.0.0.1:5001/toxic_classification", {
+    const response = await fetch("http://127.0.0.1:5000/toxic_classification", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ text: commentText }),
